a0f089a49abbed527a2711b7a412d55ca3f98586
Daniel Moretz <dcmoretz00@gmail.com>
2023-07-17 11:30:03 -0400

    Made changes to file to modify the layout of the quiz

--- script.js
+++ script.js
@@ -1,78 +1,85 @@
-// Define your questions and answers as an array of objects
-const questions = [
-    {
-      question: "Question 1:",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "Choice 2"
-    },
-    {
-      question: "Question 2",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "Choice 3"
-    },
-    // Add more questions as needed
-  ];
-  
-  // Define variables to track the state of the quiz
-  let currentQuestionIndex = 0;
-  let time = 60; // Total time in seconds
-  let timerInterval;
-  
-  // Function to start the quiz
-  function startQuiz() {
-    // Hide the start screen and show the question screen
-    document.getElementById("start-screen").classList.add("hide");
-    document.getElementById("question-screen").classList.remove("hide");
-    // Start the timer
-    timerInterval = setInterval(function() {
-      time--;
-      // Update the timer display
-      // TODO: Update the HTML element to display the remaining time
-      if (time <= 0) {
-        endQuiz();
-      }
-    }, 1000);
-    // Display the first question
-    showQuestion();
-  }
-  
-  // Function to display a question and its choices
-  function showQuestion() {
-    const currentQuestion = questions[currentQuestionIndex];
-    // TODO: Update the HTML elements to display the current question and its choices
-  }
-  
-  // Function to check the selected answer
-  function checkAnswer(selectedChoice) {
-    const currentQuestion = questions[currentQuestionIndex];
-    if (selectedChoice === currentQuestion.answer) {
-      // TODO: Handle the correct answer
-    } else {
-      // TODO: Handle the incorrect answer (subtract time, etc.)
-    }
-    // Move to the next question
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-      showQuestion();
-    } else {
-      endQuiz();
-    }
-  }
-  
-  // Function to end the quiz
-  function endQuiz() {
-    clearInterval(timerInterval);
-    // Hide the question screen and show the end screen
-    document.getElementById("question-screen").classList.add("hide");
-    document.getElementById("end-screen").classList.remove("hide");
-    // TODO: Handle the submission of initials and score
-  }
-  
-  // Add event listener to start the quiz when the button is clicked
-  document.getElementById("start-btn").addEventListener("click", startQuiz);
-  // Add event listener to check the answer when a choice is clicked
-  document.getElementById("choices").addEventListener("click", function(event) {
-    const selectedChoice = event.target.textContent;
-    checkAnswer(selectedChoice);
-  });
-  
+*, *::before, *::after {
+  box-sizing: border-box;
+  font-family: Gotham Rounded;
+}
+
+:root {
+  --hue-neutral: 200;
+  --hue-wrong: 0;
+  --hue-correct: 145;
+}
+
+body {
+  --hue: var(--hue-neutral);
+  padding: 0;
+  margin: 0;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: hsl(var(--hue), 100%, 20%);
+}
+
+body.correct {
+  --hue: var(--hue-correct);
+}
+
+body.wrong {
+  --hue: var(--hue-wrong);
+}
+
+.container {
+  width: 800px;
+  max-width: 80%;
+  background-color: white;
+  border-radius: 5px;
+  padding: 10px;
+  box-shadow: 0 0 10px 2px;
+}
+
+.btn-grid {
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  gap: 10px;
+  margin: 20px 0;
+}
+
+.btn {
+  --hue: var(--hue-neutral);
+  border: 1px solid hsl(var(--hue), 100%, 30%);
+  background-color: hsl(var(--hue), 100%, 50%);
+  border-radius: 5px;
+  padding: 5px 10px;
+  color: white;
+  outline: none;
+}
+
+.btn:hover {
+  border-color: black;
+}
+
+.btn.correct {
+  --hue: var(--hue-correct);
+  color: black;
+}
+
+.btn.wrong {
+  --hue: var(--hue-wrong);
+}
+
+.start-btn, .next-btn {
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding: 10px 20px;
+}
+
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.hide {
+  display: none;
+}
